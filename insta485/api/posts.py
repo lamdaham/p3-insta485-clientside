123ccac4a18e558a41ab181c9b83fe774ee69b66
ivanlam <ivanlam@umich.edu>
2025-02-10 21:35:17 -0500

    added posts.py

--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -2,31 +2,42 @@
 import flask
 import insta485
 
-
-@insta485.app.route('/api/v1/posts/<int:postid_url_slug>/')
-def get_post(postid_url_slug):
-    """Return post on postid.
-
-    Example:
-    {
-      "created": "2017-09-28 04:33:28",
-      "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
-      "owner": "awdeorio",
-      "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
-      "ownerShowUrl": "/users/awdeorio/",
-      "postShowUrl": "/posts/1/",
-      "postid": 1,
-      "url": "/api/v1/posts/1/"
-    }
-    """
+@insta485.app.route('/api/v1/', methods=['GET'])
+def get_api():
+    """Return service metadata."""
     context = {
-        "created": "2017-09-28 04:33:28",
-        "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
-        "owner": "awdeorio",
-        "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
-        "ownerShowUrl": "/users/awdeorio/",
-        "postShowUrl": f"/posts/{postid_url_slug}/",
-        "postid": postid_url_slug,
-        "url": flask.request.path,
+        "comments": "/api/v1/comments/",
+        "likes": "/api/v1/likes/",
+        "posts": "/api/v1/posts/",
+        "url": "/api/v1/"
     }
     return flask.jsonify(**context)
+
+
+# @insta485.app.route('/api/v1/posts/<int:postid_url_slug>/')
+# def get_post(postid_url_slug):
+#     """Return post on postid.
+
+#     Example:
+#     {
+#       "created": "2017-09-28 04:33:28",
+#       "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
+#       "owner": "awdeorio",
+#       "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
+#       "ownerShowUrl": "/users/awdeorio/",
+#       "postShowUrl": "/posts/1/",
+#       "postid": 1,
+#       "url": "/api/v1/posts/1/"
+#     }
+#     """
+#     context = {
+#         "created": "2017-09-28 04:33:28",
+#         "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
+#         "owner": "awdeorio",
+#         "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
+#         "ownerShowUrl": "/users/awdeorio/",
+#         "postShowUrl": f"/posts/{postid_url_slug}/",
+#         "postid": postid_url_slug,
+#         "url": flask.request.path,
+#     }
+#     return flask.jsonify(**context)
